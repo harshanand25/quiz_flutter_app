98eecb3a2b1e1d2d362e2124f75b474f232bb236
Harsh Anand <harsh_anand2001@outlook.com>
2023-05-21 22:40:57 +0530

    quiz.dart and questions_screen.dart file created and added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/start_screen.dart';
+import 'package:meal_app/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(225, 78, 13, 151),
-                Color.fromARGB(225, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.topRight,
-            ),
-          ),
-          child: const StartScreen()),
-    ),
-  ));
+  runApp(const Quiz());
 }
