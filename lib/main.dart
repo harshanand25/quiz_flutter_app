41584f178a4d5958cf451e6292d479a0b4ce1656
Harsh Anand <harsh_anand2001@outlook.com>
2023-05-24 21:52:51 +0530

    modified on 24th may 2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/quiz.dart';
+import 'package:meal_app/start_screen.dart';
 
 void main() {
   runApp(const Quiz());
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/questions_screen.dart';
 import 'package:meal_app/start_screen.dart';
+import 'package:meal_app/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -10,6 +12,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -25,7 +41,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
