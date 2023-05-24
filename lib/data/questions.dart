82804df326e4dd4009e6d978ca97011509544876
Harsh Anand <harsh_anand2001@outlook.com>
2023-05-24 22:19:45 +0530

    questions added

---
+++ lib/data/questions.dart
@@ -0,0 +1,13 @@
+import 'package:meal_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UI?',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  )
+];
